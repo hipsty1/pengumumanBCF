c5ad61b729a91021ba9c683301a265a55ef973e3
hipsty1 <123240066@student.upnyk.ac.id>
2025-07-24 16:23:13 +0700

    update data

--- script/data.js
+++ script/data.js
@@ -1,5 +1,5 @@
 const dataPeserta = [
-  { id: "123456", nama: "Budi Santoso", posisi: "Frontend Developer", status: "LULUS" },
-  { id: "654321", nama: "Siti Aminah", posisi: "UI/UX Designer", status: "TIDAK LULUS" },
-  { id: "202401", nama: "Agus Prasetyo", posisi: "Data Analyst", status: "LULUS" }
+    { id: "1", nama: "Budi Santoso", posisi: "Frontend Developer", status: "LULUS" },
+    { id: "2", nama: "Siti Aminah", posisi: "UI/UX Designer", status: "TIDAK LULUS" },
+    { id: "202401", nama: "Agus Prasetyo", posisi: "Data Analyst", status: "LULUS" }
 ];
