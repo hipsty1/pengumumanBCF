0d04d730458360b31720004d8f52a1f4d730f837
Hendri Prasetyo <123240066@student.upnyk.ac.id>
2025-07-27 06:54:31 +0700

    Update data.js

--- script/data.js
+++ script/data.js
@@ -1,3 +1,4 @@
 const dataPeserta = [
-    { id: "10001", nama: "Muhammad Fathir Fahrezy", posisi: "Ketua Pelaksana", status: "LULUS" }
+    { id: "10001", nama: "Muhammad Fathir Fahrezy", posisi: "Ketua Pelaksana", status: "LULUS" },
+    { id: "10002", nama: "Cristiano Ronaldo", posisi: "Ketua Pelaksana", status: "TIDAK LULUS" }
 ]
