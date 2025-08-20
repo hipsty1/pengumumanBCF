2aa8b095a3ae8e1d54b96bebbeed118a6b80a35d
Hendri Prasetyo <123240066@student.upnyk.ac.id>
2025-08-20 22:57:40 +0700

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,19 +1,19 @@
 function cekKelulusan() {
-    const input = document.getElementById("inputPeserta").value.trim().toLowerCase();
-    
-    const peserta = dataPeserta.find(p =>
-        p.id.toLowerCase() === input || p.nama.toLowerCase() === input
-    );
-    if(!peserta){
-        window.location.href = "salah.html";
-    }else{
-        if (peserta && peserta.status === "Lulus") {
-            const url = `lolos.html?nama=${encodeURIComponent(peserta.nama)}&posisi=${encodeURIComponent(peserta.posisi)}`;
-            window.location.href = url;
-        } else if (peserta && peserta.status === "Tidak Lulus") {
-            const url = `gagal.html?nama=${encodeURIComponent(peserta.nama)}`;
-            window.location.href = url;
-        }
+    const input = document.getElementById("inputPeserta").value.trim();
+    if (!input) {
+        alert("Silakan masukkan ID terlebih dahulu!");
+        return;
     }
 
+    const peserta = dataPeserta.find(p => p.id === input);
+
+    if (!peserta) {
+        alert("ID tidak ditemukan!");
+    } else if (peserta.status === "Lulus") {
+        const url = `lolos.html?nama=${encodeURIComponent(peserta.nama)}&posisi=${encodeURIComponent(peserta.posisi)}`;
+        window.location.href = url;
+    } else {
+        const url = `gagal.html?nama=${encodeURIComponent(peserta.nama)}`;
+        window.location.href = url;
+    }
 }
